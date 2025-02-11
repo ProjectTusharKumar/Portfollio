9c38a3360773f2344dc6c6f4aefd2781c8b72006
Tushar Kumar <160735794+ProjectTusharKumar@users.noreply.github.com>
2025-02-11 12:43:27 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "gist.githubusercontent.com",
+      },
+    ],
+    dangerouslyAllowSVG: true,
+  },
+};
 
 export default nextConfig;
